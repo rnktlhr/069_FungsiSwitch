b9c42084c8c02bd456dbb1dc031d69dad125f3ac
rnktlhr <rinakitluhur10@gmail.com>
2024-03-25 14:23:46 +0700

    menambah input

--- ContohPertama.cpp
+++ ContohPertama.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int bilSatu, bilDua;
+
+void input(){
+    cout << "Masukkan Bilangan Pertama = ";
+    cin >> bilSatu;
+    cout << "Masukkan Bilangan Kedua = ";
+    cin >> bilDua;
+}
+
+
+
+
 int main(){
-    
+
 }
