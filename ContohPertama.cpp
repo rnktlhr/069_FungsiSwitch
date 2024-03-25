672163df95b2ffce851f48d91e6e1fedcf20e460
rnktlhr <rinakitluhur10@gmail.com>
2024-03-25 14:32:11 +0700

    menampilkan menu

--- ContohPertama.cpp
+++ ContohPertama.cpp
@@ -27,5 +27,14 @@ float pengurangan(float a, float b){
 }
 
 int main(){
+    int pilihan;
 
+    do{
+        cout << "MENU" << endl;
+        cout << "1. penjumlahan " << endl;
+        cout << "2. pembagian " << endl;
+        cout << "3. perkalian " << endl;
+        cout << "4. pengurangan " << endl;
+        cout << "5. exit " << endl;
+   }while (pilihan  != 5);
 }
